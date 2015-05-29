2fd8e79983f2fe2a9b8d14367e1c5fef6b46e1f2
Yu Kitazume <u.kitazume@gmail.com>
2015-05-29 14:41:48 +0900

    in eager loader

--- config/initializers/hoge.rb
+++ config/initializers/hoge.rb
@@ -1,8 +1,8 @@
 require 'yaml'
 
-#Rails.application.config.before_eager_load do
+Rails.application.config.before_eager_load do
   class Hoge
     HOGE = Rails.application.secrets.hoge_key
     fail 'hogehogge' if HOGE.nil?
   end
-#end
+end
