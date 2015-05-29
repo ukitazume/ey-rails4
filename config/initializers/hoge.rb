a75292a5f1096c25109d9e591e0347f2803c2586
Yu Kitazume <u.kitazume@gmail.com>
2015-05-29 15:55:09 +0900

    debug

--- config/initializers/hoge.rb
+++ config/initializers/hoge.rb
@@ -2,8 +2,8 @@ require 'yaml'
 
 Rails.application.config.before_configuration do
   p 'ykitazume init'
-  if File.exists?("/data/eyrails/shared/config/custom.env")
-    Dotenv.load('/data/eyrails/shared/config/custom.env')
+  if File.exists?("/data/eyrails/shared/config/envcustom")
+    Dotenv.load('/data/eyrails/shared/config/env.custom')
   end
   p ENV['HOGE']
   p ENV['RAVEN']
