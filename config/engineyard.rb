310b06333e0011113682e884f43c492790cc7f09
Yu Kitazume <u.kitazume@gmail.com>
2015-05-29 15:24:38 +0900

    change before config

--- config/engineyard.rb
+++ config/engineyard.rb
@@ -1,4 +1,4 @@
-Rails.application.config.before_initialize do
+Rails.application.config.before_configuration do
   if File.exists?("/data/eyrails/shared/config/custom.env")
     Dotenv.load('/data/eyrails/shared/config/custom.env')
   end
--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,3 +1,3 @@
 Raven.configure do |config|
-    config.dsn = Rails.application.secrets.raven
+  config.dsn = Rails.application.secrets.raven
 end
