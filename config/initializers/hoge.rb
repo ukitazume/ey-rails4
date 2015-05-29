0c5e3f3dbe045b9289036b4dafb72c56284047c2
Yu Kitazume <u.kitazume@gmail.com>
2015-05-29 13:23:09 +0900

    hoge

--- config/initializers/hoge.rb
+++ config/initializers/hoge.rb
@@ -1,2 +1,4 @@
-hoge = Rails.application.secrets.hoge_key
-fail 'hogehogge' if hoge.nil?
+Rails.application.config.middleware.use "" do
+  hoge = Rails.application.secrets.hoge_key
+  fail 'hogehogge' if hoge.nil?
+end
