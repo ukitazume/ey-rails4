1b10cc4d645b9313e42c0818aa7dc72f5a92dfe8
Yu Kitazume <u.kitazume@gmail.com>
2015-05-29 15:39:40 +0900

    debug

--- config/initializers/hoge.rb
+++ config/initializers/hoge.rb
@@ -1,5 +1,18 @@
 require 'yaml'
 
+Rails.application.config.before_initialize do
+  class Hoge1
+    HOGE = Rails.application.secrets.hoge_key
+    fail 'hogehogge' if HOGE.nil?
+  end
+end
+
+Rails.application.config.to_prepare do
+  class Hoge2
+    HOGE = Rails.application.secrets.hoge_key
+    fail 'hogehogge' if HOGE.nil?
+  end
+end
 Rails.application.config.before_eager_load do
   class Hoge
     HOGE = Rails.application.secrets.hoge_key
--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,3 +1,4 @@
 Raven.configure do |config|
+  p Rails.application.secrets.raven  
   config.dsn = Rails.application.secrets.raven
 end
