6f0cf6c4f96abeef0cb06b629770c05cd0f0d7e3
Yu Kitazume <u.kitazume@gmail.com>
2015-05-29 15:42:52 +0900

    debug

--- config/initializers/hoge.rb
+++ config/initializers/hoge.rb
@@ -1,5 +1,11 @@
 require 'yaml'
 
+Rails.application.config.before_configuration do
+  class Hoge0
+    HOGE = Rails.application.secrets.hoge_key
+    fail 'hogehogge' if HOGE.nil?
+  end
+end
 Rails.application.config.before_initialize do
   class Hoge1
     HOGE = Rails.application.secrets.hoge_key
