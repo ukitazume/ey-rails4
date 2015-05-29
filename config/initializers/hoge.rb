98903f0f0c32a2ba0c82d0a3aa44a6cd319002d3
Yu Kitazume <u.kitazume@gmail.com>
2015-05-29 15:50:34 +0900

    debug

--- config/initializers/hoge.rb
+++ config/initializers/hoge.rb
@@ -1,6 +1,7 @@
 require 'yaml'
 
 Rails.application.config.before_configuration do
+  p 'ykitazume init'
   if File.exists?("/data/eyrails/shared/config/custom.env")
     Dotenv.load('/data/eyrails/shared/config/custom.env')
   end
