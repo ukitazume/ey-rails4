01f4aaf21156d2e50fe5d1a185a40f969bb8e4ae
Yu Kitazume <u.kitazume@gmail.com>
2015-05-29 14:14:37 +0900

    with pretend

--- config/initializers/hoge.rb
+++ config/initializers/hoge.rb
@@ -1,5 +1,6 @@
-require 'yaml'
-class Hoge
-  hoge = Rails.application.secrets.hoge_key
-  fail 'hogehogge' if hoge.nil?
+Rails.application.config.to_prepare do
+  class Hoge
+    hoge = Rails.application.secrets.hoge_key
+    fail 'hogehogge' if hoge.nil?
+  end
 end
